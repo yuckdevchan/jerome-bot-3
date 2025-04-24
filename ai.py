2febba33ce54c78d3ba258e6dcecfa3823a658b3
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2025-04-24 20:56:05 +0100

    stuff

--- ai.py
+++ ai.py
@@ -1,35 +1,40 @@
-import subprocess
+import subprocess, random
 from pathlib import Path
 
-def build_modelfiles():
+from config import config
+
+def build_model_files() -> list:
+    models = []
     i = 0
     for item in Path("models").iterdir():
         print(f"⚙️ Building model: '{item}'")
         if item.name.endswith(".ModelFile"):
+            models.append(item.name.split(".")[0])
             i += 1
             subprocess.run(["/usr/local/bin/ollama", "create", item.name.split(".")[0], "-f", "models/" + item.name])
-
     print("------")
     print(f"✅ Built {i} ModelFiles")
+    return models
 
 def get_ai_response(prompt: str, model: str) -> str:
     try:
-        if not prompt: prompt = "Hello you stupid bot, I'm very clever and very funny!"
+        if not prompt: prompt = "I don't care about you."
         response = subprocess.run(
             ["/usr/local/bin/ollama", "run", model, prompt], 
             capture_output=True, 
             text=True,
             check=True
         )
-        print(f"Running: /usr/local/bin/ollama run {model} {prompt}")
         response_text = response.stdout.strip()
         if response_text == "":
             response_text = "I simply have no words."
+        if model.endswith("_angry"):
+            response_text = response_text.upper()
         return response_text
     except subprocess.CalledProcessError as e:
         print(f"Command error: {e}")
         print(f"Error output: {e.stderr}")
-        return "An error occurred while processing your request :("
+        return config["speechless"]
     except Exception as e:
         print(f"Error: {e}")
-        return "An error occurred while processing your request :("
+        return config["speechless"]
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import discord, subprocess, random, json
+import discord, subprocess, random
 from pathlib import Path
 
+from config import config
 import ai
 
-with open("config.json") as f:
-    config = json.load(f)
-
-ai.build_modelfiles()
+models = ai.build_model_files()
 
 bot = discord.Bot()
+
+letter_emojis = {"A": "🇦", "B": "🇧", "C": "🇨", "D": "🇩", "E": "🇪", "F": "🇫", "G": "🇬", "H": "🇭", "I": "🇮", "J": "🇯",}
 
 @bot.event
 async def on_ready():
@@ -16,41 +16,71 @@ async def on_ready():
     print("------")
     bot_presence = await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=config["activity"]["name"], url=config["activity"]["url"]))
 
+async def react(message, angry=False):
+    try:
+        if angry:
+            await message.add_reaction(random.choice([
+                "😡", "😠", "🤬", "😤", "😾", "👿", "💢"
+            ]))
+        else:
+            if random.randint(0, 100) <= 20:
+                for letter in config["simple_bot_name"]:
+                    await message.add_reaction(letter_emojis[letter].upper())
+                await message.add_reaction("👍")
+            else:
+                await message.add_reaction(random.choice([
+                    "🫡", "🫰", "👌", "🤌", "🙆"
+                ]))
+    except:
+        pass
+
+async def reply(message_object: discord.Message, response: str):
+    try:
+        print(f"💬  {response}")
+        await message_object.reply(response)
+    except discord.Forbidden:
+        print("❌ Permission error: Unable to send message in this channel.")
+    except discord.HTTPException as e:
+        print(f"❌ HTTP error: {e}")
+    except Exception as e:
+        print(f"❌ An unexpected error occurred: {e}")
+
 @bot.event
 async def on_message(message):
     if message.channel.id in config["ignored_channels"]:
         return
     if message.author == bot.user:
         return
+    angry = message.author.id in config["users_to_be_angry_at"]
+    message_content = message.content.replace(f"<@{bot.user.id}>", "").strip()
     if bot.user.mentioned_in(message):
-        ai_response = ai.get_ai_response(message.content.replace(f"<@{bot.user.id}>", "").strip(), model="grok")
-        if message.author.id in config["users_to_be_angry_at"]:
-            ai_response = ai_response.upper()
-        await message.reply(ai_response)
-    elif message.author.id in config["users_to_be_angry_at"] and random.randint(0, 100) <= 100:
-        await message.reply(ai.get_ai_response(message.content.replace(f"<@{bot.user.id}>", "").strip(), model="grok_shut_up").upper())
+        print(f"📝{"💢" if angry else " "} {message.author.name} - {message_content} in #{message.channel.name} - [{message.channel.id}]")
+        await react(message)
+        ai_response = ai.get_ai_response(message_content, model="jerome")
+        await reply(message, ai_response)
+    elif angry and random.randint(0, 100) <= 100:
+        await react(message, angry=True)
+        await reply(message, ai.get_ai_response(message_content, model="jerome_angry"))
     elif random.randint(0, 100) <= 1:
+        await react(message)
         if random.randint(0, 100) <= 50:
-            await message.reply(random.choice([
-                "Haha, true!",
-                "Are you being for real right now?",
-                "That's a good one!",
-                "I can't believe you just said that!",
-                "You really think so?",
-                "That's a bold statement!",
-                "I don't know about that one!",
-                "That's a hot take!",
-                "Interesting perspective!",
-                "I see where you're coming from!",
-                "That's a unique way to look at it!",
-                "True, true, true!",
-            ]))
-        if random.randint(0, 100) <= 50:
-            await message.reply(ai.get_ai_response(message.content.replace(f"<@{bot.user.id}>", "").strip(), model="grok_short"))
+            await reply(message, random.choice(config["pre_made_responses"]))
+        elif random.randint(0, 100) <= 50:
+            await reply(message, ai.get_ai_response(message_content, model="jerome_short"))
 
-@bot.command(description="Ask Grok a question")
-async def ask(ctx, *, question: str):  # Use * to capture all arguments as one string
+@bot.command(description=f"Ask {config["bot_name"]} a question")
+async def ask(ctx, *, question: str):
     await ctx.respond("Thinking...")
-    await ctx.respond(ai.get_ai_response(question, model="grok"))
+    await ctx.respond(ai.get_ai_response(question, model="jerome"))
+
+@bot.command(name="ask_model", description=f"Ask {config['bot_name']} a question using a specific model")
+async def ask_model(
+    ctx: discord.ApplicationContext,
+    model: discord.Option(str, description="The model to use", choices=models),
+    prompt: discord.Option(str, description="The prompt to send to the model"),
+    private: discord.Option(str, description="Show the response to everyone else, or just you?", choices=["Yes", "No"]),
+):
+    await ctx.respond("🤔 Thinking...", ephemeral=True)
+    await ctx.respond(ai.get_ai_response(prompt, model=model), ephemeral=private == "Yes")
 
 bot.run(open("TOKEN").read().strip())
